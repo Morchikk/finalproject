95688b5e663ee44f6b0a79f99f4e69e2def2a116
morchikk <morchikk@gmail.com>
2022-12-24 15:26:00 +0400

    home page &  about us part1

---
+++ node.js/script.js
@@ -0,0 +1,53 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+// slider
+
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = document.querySelector(".carousel").children;
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener("click", () => {
+  nextImage("next");
+});
+
+next.addEventListener("click", () => {
+  nextImage("prev");
+});
+
+function nextImage(direction) {
+  if (direction == "next") {
+    index++;
+    if (index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if (index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove("main");
+  }
+  images[index].classList.add("main");
+}
+
+
+
